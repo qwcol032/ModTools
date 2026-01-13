382bf1a8239da58b14f59de66d7270ae81f2e929
G0M <43164007+qwcol032@users.noreply.github.com>
2026-01-13 22:53:30 +0900

    v2.2.7

--- NikkeGallTools.js
+++ NikkeGallTools.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NikkeGallTools
 // @namespace    http://tampermonkey.net/
-// @version      2.2.6
+// @version      2.2.7
 // @description  니갤관리에 필요한 각종기능 모음(Edit by ManyongKim & G0M)
 // @author       ZENITH(int64) & E - ManyongKim, G0M
 // @noframes     true
@@ -25,7 +25,7 @@ https://github.com/philsturgeon/dbad/blob/master/LICENSE.md
 https://namu.wiki/w/DBAD%20%EB%9D%BC%EC%9D%B4%EC%84%A0%EC%8A%A4
 ------------------------------------------------------------------*/
 
-let toolVersion = "2.2.6";
+let toolVersion = "2.2.7";
 let flagAlert = true;
 let gallMonitorON = false;
 let FUZZY_BAN_LIST;
